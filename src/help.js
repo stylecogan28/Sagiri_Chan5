ebd088e4f26d1901cd2413511a59a8adf66bddee
stylecogan28 <76465599+stylecogan28@users.noreply.github.com>
2021-01-06 15:49:55 +0700

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,127 @@
+const help = (prefix, pushname, oname) => {
+	return`
+╭〔 _*ABOUT*_ 〕
+│❦ *${prefix}info*
+│❦ *${prefix}bugreport* [Report Bugs]
+│❦ *${prefix}runtime*
+│❦ *${prefix}join* [Link Group]
+│❦ *${prefix}owner*
+│❦ *${prefix}rate*
+ ⊢〔 _*GROUP*_ 〕
+│❦ *${prefix}clone* [@tag]
+│❦ *${prefix}promote* [@tag]
+│❦ *${prefix}demote* [@tag]
+│❦ *${prefix}tagall*
+│❦ *${prefix}tagall2*
+│❦ *${prefix}tagall3*
+│❦ *${prefix}tagall4*
+│❦ *${prefix}tagall5*
+│❦ *${prefix}simih* [1/0]
+│❦ *${prefix}group* [open/close]
+│❦ *${prefix}setdesc* [text]
+│❦ *${prefix}setpp*
+│❦ *${prefix}ownergroup*
+│❦ *${prefix}setname* [text]
+│❦ *${prefix}add* [628886060342]
+│❦ *${prefix}kick* [@tag]
+│❦ *${prefix}linkgroup*
+│❦ *${prefix}listadmin*
+│❦ *${prefix}tomp3*
+│❦ *${prefix}welcome* [1/0]
+│❦ *${prefix}closegc*
+│❦ *${prefix}opengc*
+│❦ *${prefix}hidetag*
+ ⊢〔 _*MEDIA*_ 〕
+│❦ *${prefix}toimg*
+│❦ *${prefix}ttp* [text]
+│❦ *${prefix}gtts* [kode bahasa] [text]
+│❦ *${prefix}url2img* [link]
+│❦ *${prefix}wait*
+│❦ *${prefix}ocr*
+│❦ *${prefix}nulis* [text]
+│❦ *${prefix}cekpremium*
+│❦ *${prefix}bahasa*
+│❦ *${prefix}virtex*
+│❦ *${prefix}kodenegara*
+│❦ *${prefix}blocklist*
+│❦ *${prefix}chatlist*
+│❦ *${prefix}premiumlist*
+│❦ *${prefix}meme*
+│❦ *${prefix}memeindo*
+│❦ *${prefix}ssweb* [link]
+│❦ *${prefix}exe*
+│❦ *${prefix}qrcode* [text]
+│❦ *${prefix}ping*
+│❦ *${prefix}3dtext*
+│❦ *${prefix}quotes*
+│❦ *${prefix}fml*
+│❦ *${prefix}trendtwit*
+│❦ *${prefix}truth*
+│❦ *${prefix}dare*
+│❦ *${prefix}apakah* [pertanyaan]
+│❦ *${prefix}bisakah* [pertanyaan]
+│❦ *${prefix}kapankah* [pertanyaan]
+│❦ *${prefix}wa.me*
+│❦ *${prefix}jadwaltv*
+│❦ *${prefix}cuaca* [daerah]
+│❦ *${prefix}kbbi*
+│❦ *${prefix}beritahoax*
+│❦ *${prefix}infonomor* [628886060342]
+│❦ *${prefix}bpink*
+│❦ *${prefix}jadwaltvnow*
+│❦ *${prefix}semoji*
+│❦ *${prefix}testime*
+│❦ *${prefix}artinama* [nama]
+│❦ *${prefix}map* [daerah]
+│❦ *${prefix}covid*
+│❦ *${prefix}alay* [text]
+ ⊢〔 _*MUSIC*_ 〕
+│❦ *${prefix}lirik* [nama lagu]
+│❦ *${prefix}chord* [nama lagu]
+ ⊢〔 _*STALK*_ 〕
+│❦ *${prefix}igstalk* [@username]
+ ⊢〔 _*EDUCATION*_ 〕
+│❦ *${prefix}wiki* [kata kunci]
+│❦ *${prefix}brainly* [pertanyaan]
+ ⊢〔 _*ISLAM*_ 〕
+│❦ *${prefix}jsholat* [daerah]
+│❦ *${prefix}quran*
+ ⊢〔 _*OWNER*_ 〕
+│❦ *${prefix}setprefix* [prefix]
+│❦ *${prefix}oadd* [628886060342]
+│❦ *${prefix}okick* [@tag]
+│❦ *${prefix}bc* [text]
+│❦ *${prefix}setppbot*
+│❦ *${prefix}clone* [@tag]
+│❦ *${prefix}clearall*
+│❦ *${prefix}bcgc* [text]
+│❦ *${prefix}osimih* [1/0]
+│❦ *${prefix}block* [@tag]
+│❦ *${prefix}unblock* [@tag]
+│❦ *${prefix}leave*
+ ⊢〔 _*MAKER*_ 〕
+│❦ *${prefix}sticker*
+│❦ *${prefix}shortLink* [link]*
+│❦ *${prefix}quotemaker*
+│❦ *${prefix}glitch* [Nazwa|Chanz]
+│❦ *${prefix}send* [Nazwa|Chanz]
+ ⊢〔 _*SPAM*_ 〕
+│❦ *${prefix}spamcall* [8886060342]
+│❦ *${prefix}
+ ⊢〔 _*WIBU*_ 〕
+│❦ *${prefix}animecry*
+│❦ *${prefix}animehug*
+│❦ *${prefix}neonime*
+│❦ *${prefix}pokemon*
+│❦ *${prefix}nekonime*
+ ⊢〔 _*DOWNLOADER*_ 〕
+│❦ *${prefix}ytmp3*
+│❦ *${prefix}ytmp4*
+│❦ *${prefix}tiktok* [link]
+│❦ *${prefix}joox* [link]
+│❦ *${prefix}fb* [link]
+│❦ *${prefix}pinterest* [link]
+╰〔 _*©CopyRight 2021 by ${oname}*_ 〕`
+}
+
+exports.help = help
